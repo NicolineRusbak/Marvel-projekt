d60895121b6f85e09122cd4a0eea29cc070facf8
NicolineRusbak <57627700+NicolineRusbak@users.noreply.github.com>
2020-09-16 10:15:36 +0200

    Add files via upload

---
+++ routes/characters.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Character = require('../models/character');
+
+router.get('/', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    //res.send(JSON.stringify({message: 'GET /api/authors'}));
+    try {
+        const characters = await Character.readAll();
+        res.send(JSON.stringify(characters));
+    }
+    catch (err) {
+        res.status(418).send(JSON.stringify(err));
+    }
+});
+
+router.get('/:characId', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    //res.send(JSON.stringify({message: 'GET /api/authors/:authorId'}));
+    const paramsTestObject = {
+        characId: req.params.characId
+    }
+    const {error} = Character.validate(paramsTestObject);
+    if (error) {
+        res.status(400).send(JSON.stringify(error));
+    } else {
+        try {
+            const character = await Character.readById(req.params.characId);
+            res.send(JSON.stringify(character));
+        }
+        catch (err){
+            res.status(418).send(JSON.stringify(err));
+        }
+    }
+});
+
+module.exports = router;
