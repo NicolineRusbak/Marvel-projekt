c555871c9a884c9450a2c7e009960047e14fcb09
liney82 <lineyjonasdottir@gmail.com>
2020-09-17 14:39:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const characters = require('./routes/characters'); // books
+const movies = require('./routes/movies'); // movies
 
 const app = express();
-const myPort = 8543;
+const myPort = ;
 
 app.use(cors());    // to enable cross origin resource sharing when accessed from browser
+app.use(setJSON); // Hvad gør den her?
 app.use(express.json());    // to process the incoming request (request body MUST be in json format) and create the req.body object for later use
+
+
 app.use('/api/characters', characters);
+app.use('/api/movies', movies);
 
 app.listen(myPort, () => console.log(`Listening on port ${myPort}...`));
