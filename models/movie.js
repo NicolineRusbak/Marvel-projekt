17fff16ca92f8c9b6cf18cf2dbe1edd51220bbf8
NicolineRusbak <1081543@ucn.dk>
2020-09-23 17:38:11 +0200

    create movie

--- models/movie.js
+++ models/movie.js
@@ -6,7 +6,7 @@ class Movie {
     // constructor
     constructor(movieObj) {
         this.movieId = movieObj.movieId;
-        this.movieTitle = movieObj.movieId;
+        this.movieTitle = movieObj.movieTitle;
         this.movieDescription = movieObj.movieDescription;
         this.movieReleaseYear = movieObj.movieReleaseYear;
     }
@@ -87,6 +87,52 @@ class Movie {
                     if (error) throw error;
 
                     resolve(new Movie(record));
+                }
+                catch (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                sql.close();
+            })();
+        });
+    }
+
+    // create a new movie in the db
+    create() {
+        return new Promise((resolve, reject) => {
+            (async () => {
+                // connect to the DB
+                // insert the new author into the DB
+                //      read out the newly inserted author form the DB
+                // validate that the result from the DB is compliant with the Author object's format
+                // if all good then convert the result from DB (the new author) into an Author object
+                //      resolve(Author)
+                // CLOSE THE DB CONNECTION
+                try {
+                    const pool = await sql.connect(con);
+                    const result = await pool.request()
+                        .input('movieTitle', sql.NVarChar(50), this.movieTitle) // title
+                        .input('movieDescription', sql.NVarChar(1000), this.movieDescription) // description
+                        .input('movieReleaseYear', sql.Int, this.movieReleaseYear) // release year
+                        .query(`INSERT INTO marvelMovie (movieTitle, movieDescription, movieReleaseYear) 
+                                VALUES (@movieTitle, @movieDescription, @movieReleaseYear); 
+                                SELECT * FROM marvelMovie WHERE movieID = SCOPE_IDENTITY()`);
+
+                    console.log(result);
+                    if (!result.recordset[0]) throw { message: 'Movie not found. Failed to save Movie to database.' };
+
+                    const record = {
+                        movieId: result.recordset[0].movieID, //movieId
+                        movieTitle: result.recordset[0].movieTitle, //title
+                        movieDescription: result.recordset[0].movieDescription, // description
+                        movieReleaseYear: result.recordset[0].movieReleaseYear // release year
+                    }
+
+                    const { error } = Movie.validate(record); // Movie
+                    if (error) throw error;
+
+                    resolve(new Movie(record));
+
                 }
                 catch (err) {
                     console.log(err);
