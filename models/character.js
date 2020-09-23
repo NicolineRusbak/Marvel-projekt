46fd94546b4b975e593c516b4be6380c989d8f41
NicolineRusbak <1081543@ucn.dk>
2020-09-23 20:53:47 +0200

    create character

--- models/character.js
+++ models/character.js
@@ -142,7 +142,61 @@ class Character {
         });
     } 
 
-    // create slettet men kan tages fra Library-version5
+    // create a new character in the db
+    create() {
+        return new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    const pool = await sql.connect(con);
+                    const result = await pool.request()
+                        .input('characFirstName', sql.NVarChar(50), this.characFirstName) 
+                        .input('characLastName', sql.NVarChar(50), this.characLastName) 
+                        .input('characAlias', sql.NVarChar(50), this.characAlias)
+                        .input('characDateOfBirth', sql.NVarChar(10), this.characDateOfBirth)
+                        .input('characGender', sql.NVarChar(10), this.characGender)
+                        .input('characJob', sql.NVarChar(50), this.characJob)
+                        .input('characOrigin', sql.NVarChar(50), this.characOrigin)
+                        .input('characAbility', sql.NVarChar(50), this.characAbility)
+                        .input('characWeakness', sql.NVarChar(50), this.characWeakness)
+                        .input('characArtefact', sql.NVarChar(50), this.characArtefact)
+                        .input('characActor', sql.NVarChar(50), this.characActor)
+
+                        .query(`INSERT INTO marvelCharacter (characFirstName, characLastName, characAlias, characDateOfBirth, characGender, characJob, characOrigin, characAbility, characWeakness, characArtefact, characActor) 
+                                VALUES (@characFirstName, @characLastName, @characAlias, @characDateOfBirth, @characGender, @characJob, @characOrigin, @characAbility, @characWeakness, @characArtefact, @characActor); 
+                                SELECT * FROM marvelCharacter WHERE characID = SCOPE_IDENTITY()`);
+
+                    console.log(result);
+                    if (!result.recordset[0]) throw { message: 'Character not found. Failed to save Character to database.' };
+
+                    const record = {
+                        characId: result.recordset[0].characID,
+                        characFirstName: result.recordset[0].characFirstName,
+                        characLastName: result.recordset[0].characLastName,
+                        characAlias: result.recordset[0].characAlias,
+                        characDateOfBirth: result.recordset[0].characDateOfBirth,
+                        characGender: result.recordset[0].characGender,
+                        characJob: result.recordset[0].characJob,
+                        characOrigin: result.recordset[0].characOrigin,
+                        characAbility: result.recordset[0].characAbility,
+                        characWeakness: result.recordset[0].characWeakness,
+                        characArtefact: result.recordset[0].characArtefact,
+                        characActor: result.recordset[0].characActor
+                    }
+
+                    const { error } = Character.validate(record); 
+                    if (error) throw error;
+
+                    resolve(new Character(record));
+
+                }
+                catch (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                sql.close();
+            })();
+        });
+    }
 
 }
 
--- routes/characters.js
+++ routes/characters.js
@@ -35,4 +35,20 @@ router.get('/:characId', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => { //[auth, admin] inden async
+    res.setHeader('Content-Type', 'application/json');
+    const { error } = Character.validate(req.body);
+    if (error) {
+        res.status(400).send(JSON.stringify(error));
+    } else {
+        try {
+            const character = await new Character(req.body).create();
+            res.send(JSON.stringify(character));
+        }
+        catch (err) {
+            res.status(418).send(JSON.stringify(err));
+        }
+    }
+});
+
 module.exports = router;
