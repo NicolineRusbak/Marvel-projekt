1556378ee1ec9d77aff20c9cfcdd1dfba7132d74
liney82 <57624182+liney82@users.noreply.github.com>
2020-09-25 14:40:16 +0200

    Add files via upload

--- marvel-frontend/script.js
+++ marvel-frontend/script.js
@@ -1,19 +1,22 @@
 const btnListAll = document.querySelector('#listAll');
 const divOutput = document.querySelector('#output');
+const btnListAllMovies = document.querySelector('#listAllMovies');
+const divOutputMovie = document.querySelector('#outputMovie');
+
 
 btnListAll.addEventListener('click', (e) => {
     const xhttp = new XMLHttpRequest(); 
     // XMLHttpRequest() er det der 'snakker' med serveren (library-api). 
     // "The XMLHttpRequest object can be used to exchange data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page."
     xhttp.onreadystatechange = function () { // hvis der sker ændringer på readystate køres funktionen
-        if (this.readyState == 4 && this.status == 200){
+        if (this.readyState == 4 && this.status == 200) {
             const response = JSON.parse(this.responseText);
             divOutput.innerHTML = '';
-            for(let i = 0; i < response.length; i++){
+            for(let i = 0; i < response.length; i++) {
                 //divOutput.innerHTML += `${response[i].title}<br/>`;
                 const divCharacter = document.createElement('div');
                 divCharacter.innerHTML = `${response[i].characAlias}`;   
-                divCharacter.addEventListener('click', () => { characterOnClickHandle(response[i].characId)} ); 
+                divCharacter.addEventListener('click', () => { characterOnClickHandle(response[i].characId)}); 
                 divOutput.appendChild(divCharacter);              
             }
         };
@@ -21,7 +24,7 @@ btnListAll.addEventListener('click', (e) => {
             divOutput.innerHTML = 'Ooops something went wrong.';
         }
     }
-    xhttp.open('GET', 'http://127.0.0.1:8543/api/characters', true);
+    xhttp.open('GET', 'http://127.0.0.1:8558/api/characters', true);
     xhttp.send();
     // Open(): Specifies the type of request
     // send(): Sends the request to the server (used for GET)
@@ -34,9 +37,50 @@ function characterOnClickHandle(characId) {
             divOutput.innerHTML = this.responseText;
         }
         if (this.readyState == 4 && this.status > 400) {
-            divOutput.innerHTML = 'Ooops something went wrong.';
+            divOutput.innerHTML = 'Ooops something went wrong.'
+
         }
     }
-    xhttp.open('GET', `http://127.0.0.1:8543/api/characters/${characId}`, true);
+    xhttp.open('GET', `http://127.0.0.1:8558/api/characters/${characId}`, true);
+    xhttp.send();
+}
+
+btnListAllMovies.addEventListener('click', (e) => {
+    const xhttp = new XMLHttpRequest(); 
+    // XMLHttpRequest() er det der 'snakker' med serveren (library-api). 
+    // "The XMLHttpRequest object can be used to exchange data with a web server behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page."
+    xhttp.onreadystatechange = function () { // hvis der sker ændringer på readystate køres funktionen
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(this.responseText);
+            divOutputMovie.innerHTML = '';
+            for(let i = 0; i < response.length; i++){
+                //divOutput.innerHTML += `${response[i].title}<br/>`;
+                const divMovie = document.createElement('div');
+                divMovie.innerHTML = `${response[i].movieTitle}`;   
+                divMovie.addEventListener('click', () => { movieOnClickHandle(response[i].movieId)}); 
+                divOutputMovie.appendChild(divMovie);              
+            }
+        };
+        if (this.readyState == 4 && this.status > 400) {
+            divOutputMovie.innerHTML = 'Ooops something went wrong.';
+        }
+    }
+    xhttp.open('GET', 'http://127.0.0.1:8558/api/movies', true);
+    xhttp.send();
+    // Open(): Specifies the type of request
+    // send(): Sends the request to the server (used for GET)
+});
+
+function movieOnClickHandle(movieId) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            divOutputMovie.innerHTML = this.responseText;
+        }
+        if (this.readyState == 4 && this.status > 400) {
+            divOutputMovie.innerHTML = 'Ooops something went wrong.';
+        }
+    }
+    xhttp.open('GET', `http://127.0.0.1:8558/api/movies/${movieId}`, true);
     xhttp.send();
 }
